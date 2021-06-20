f4d7120010495332ee5df60140843ae5b6d2245d
Andrea Arsic <andrea.arsic88@gmai.com>
2021-06-20 14:15:53 +0200

    Modify BasicPage

--- src/pages/BasicPage.java
+++ src/pages/BasicPage.java
@@ -1,15 +1,27 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 public abstract class BasicPage {
 
 protected WebDriver driver;
-	
-	public BasicPage(WebDriver driver) {
+protected JavascriptExecutor js;
+
+	public BasicPage(WebDriver driver, JavascriptExecutor js) {
 		this.driver = driver;
+		this.js = js;
 	}
 	
-	
+	public boolean elementExist(By by) {
+		boolean exist = true;
+		try {
+			driver.findElement(by);
+		} catch (Exception e){
+			exist = false;
+		}
+		return exist;
+	}
 	
 }
