5975bf5a0d0bebaabda60acdb02cc4cc4a341e11
Andrea Arsic <andrea.arsic88@gmai.com>
2021-06-22 03:18:16 +0200

    Add AuthPage

---
+++ src/pages/AuthPage.java
@@ -0,0 +1,39 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AuthPage extends BasicPage{
+
+	public AuthPage(WebDriver driver, JavascriptExecutor js, WebDriverWait wait) {
+		super(driver, js, wait);
+	}
+	
+//	●	get metode za sve potrebne elemente sa stranice   
+//	●	metodu koja odjavljuje korisnika sa sistema
+	
+	public WebElement getAccountfield() {
+		return driver.findElement(By.className("filled "));
+	}
+	
+	public WebElement getMyAccount() {
+		return driver.findElement(By.xpath("//*[class='my-account-dropdown'/ul/li/a"));
+	}
+	
+	public WebElement getLogout() {
+		return driver.findElement(By.xpath("//*[class='my-account-dropdown'/ul/li[2]/a"));
+	}
+	
+	public void logout() {
+		getAccountfield().click();
+		getLogout().click();
+	}
+	
+	
+
+
+	
+}
